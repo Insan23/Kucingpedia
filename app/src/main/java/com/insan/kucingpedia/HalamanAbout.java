ddaeefdf1d86c61baaed9f8ae4edf1cb1c0fae90
insan23 <green.blaze235@gmail.com>
2019-10-15 13:07:07 +0700

    Initial Commit

---
+++ app/src/main/java/com/insan/kucingpedia/HalamanAbout.java
@@ -0,0 +1,14 @@
+package com.insan.kucingpedia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HalamanAbout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_halaman_about);
+    }
+}
