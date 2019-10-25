6952dd66705ee287db7e21268f0e021123bffa85
insan23 <green.blaze235@gmail.com>
2019-10-25 12:46:38 +0700

    completing add new info layout and logic

--- app/src/main/java/com/insan/kucingpedia/activities/HalamanTambah.java
+++ app/src/main/java/com/insan/kucingpedia/activities/HalamanTambah.java
@@ -61,7 +61,37 @@ public class HalamanTambah extends AppCompatActivity {
         toolbar.getNavigationIcon().setColorFilter(getResources().getColor(R.color.white), PorterDuff.Mode.SRC_ATOP);
         getSupportActionBar().setTitle("");
 
+        ras = findViewById(R.id.ras);
+        deskripsi = findViewById(R.id.deskripsi);
+        foto = findViewById(R.id.foto);
+        error_foto = findViewById(R.id.error_foto);
+        simpan = findViewById(R.id.simpan);
 
+        id = getIntent().getLongExtra("_id", -1);
+        if (id == -1) {
+            getSupportActionBar().setTitle("Tambah Informasi Kucing Baru");
+        } else {
+            getSupportActionBar().setTitle("Ubah Informasi Kucing");
+            ras.setText(getIntent().getStringExtra("ras"));
+            deskripsi.setText(getIntent().getStringExtra("deskripsi"));
+        }
+
+        foto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(HalamanTambah.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, BACA_MEMORI);
+                }
+                pickFromGallery();
+            }
+        });
+
+        simpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                simpanInformasi();
+            }
+        });
     }
 
     @Override
@@ -92,6 +122,78 @@ public class HalamanTambah extends AppCompatActivity {
             }
     }
 
+    private void simpanInformasi() {
+        enable(false);
+        error_foto.setText("");
+        SQLiteDatabase db = new KucingDBHelper(getApplicationContext()).getReadableDatabase();
+        boolean cek = true;
+        String teksRas = ras.getText().toString();
+        String teksDeskripsi = deskripsi.getText().toString();
 
+        if (TextUtils.isEmpty(teksRas)) {
+            cek = false;
+            enable(true);
+            ras.setError("Harus Diisi");
+        }
+
+        if (TextUtils.isEmpty(teksDeskripsi)) {
+            cek = false;
+            enable(true);
+            deskripsi.setError("Harus Diisi");
+        }
+
+        if (fotoTerpilih == null) {
+            cek = false;
+            enable(true);
+            error_foto.setText("Pilih Foto Terlebih Dahulu!");
+        }
+
+        if (cek) {
+            if (id == -1) {
+                ContentValues v = new ContentValues();
+                v.put(KucingDB.KucingEntry.COL_RAS_KUCING, teksRas);
+                v.put(KucingDB.KucingEntry.COL_DESKRIPSI, teksDeskripsi);
+                v.put(KucingDB.KucingEntry.COL_IMG, fotoTerpilih);
+                v.put(KucingDB.KucingEntry.COL_TIPE, KucingDB.TIPE_UTAMA);
+                new KucingDBOperation(db).insertData(v);
+            } else {
+                ContentValues v = new ContentValues();
+                v.put(KucingDB.KucingEntry.COL_RAS_KUCING, teksRas);
+                v.put(KucingDB.KucingEntry.COL_DESKRIPSI, teksDeskripsi);
+                v.put(KucingDB.KucingEntry.COL_IMG, fotoTerpilih);
+                v.put(KucingDB.KucingEntry.COL_TIPE, KucingDB.TIPE_UTAMA);
+                new KucingDBOperation(db).updateDataById(id, v);
+            }
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+
+                }
+            }, 3000);
+            finish();
+        }
+        db.close();
+    }
+
+    private void pickFromGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        String[] mimeTypes = {"image/jpeg", "image/png"};
+        intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
+        startActivityForResult(intent, KODE_AMBIL_FOTO);
+    }
+
+    private void enable(boolean st) {
+        ras.setEnabled(st);
+        deskripsi.setEnabled(st);
+        simpan.setEnabled(st);
+        if (st) {
+            findViewById(R.id.overlay).setVisibility(View.GONE);
+            findViewById(R.id.pro_circular).setVisibility(View.GONE);
+        } else {
+            findViewById(R.id.overlay).setVisibility(View.VISIBLE);
+            findViewById(R.id.pro_circular).setVisibility(View.VISIBLE);
+        }
+    }
 
 }
